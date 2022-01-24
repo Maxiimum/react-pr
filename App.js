8de072b679c7c0ad181734511aa723edbc1aba60
Maxiimum <80867166+Maxiimum@users.noreply.github.com>
2022-01-25 01:49:00 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './components/Mainpage.js'
+import Nav from './components/Nav.js'
+
+
+function App() {
+  return (
+    <div>
+     <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
